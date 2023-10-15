3416b61932e9f3b6e273ea4a0cae329fa775929e
osajele odegua <odeguaj@gmail.com>
2023-10-15 02:48:41 -0700

    web_stack_debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-#find out why Apache is returning a 500 error and fix
+#find out why Apache is returning a 500 error and fix it
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
